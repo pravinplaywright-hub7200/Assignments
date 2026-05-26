732ca9eb78c4ed6633c37650e48da637e5e109ef
Pravin Yadav <pravinplaywright@gmail.com>
2026-05-26 21:34:54 +0530

    Asisgnment -1

---
+++ typescript-training/langauge-basics/data-Types/assignment_1.ts
@@ -0,0 +1,44 @@
+let temperature:number =25.5;
+console.log(`Temperature of a city in : ${temperature} °C`);
+
+// 2. Whether a customer has placed an order (Boolean)
+let isOrderPlaced:boolean=true;
+console.log(`Has the customer placed an order?: ${isOrderPlaced}`);
+
+//3.Person's phone number: "123-456-7890"
+let mobileNumber:string= "123-456-7890"
+console.log(`Person's phone number: ${mobileNumber}`);
+
+//4. Amount of money in a customer's bank account: 1000.50
+let accAmount:number = 1000.50;
+console.log(`Amount of money in a customer's bank account: ${accAmount}`);
+
+//5. Person's email address: "john.doe@example.com"
+let email:string ="john.doe@example.com";
+console.log(`Person's email address: ${email}`);
+
+//6. Coordinates of a location (latitude, longitude): 37.7749, -122.4194
+interface cordinate {     
+    latitude: number ,     
+    longitude: number,     
+    }  
+let locationData:cordinate = {   
+    latitude: 37.7749,    
+    longitude: -122.4194, }; 
+    console.log("Coordinates of a location ", locationData.latitude,locationData.longitude); 
+
+//7.Person's marital status: true or false
+
+//8. Person's occupation: "Software Engineer"
+//9. Person's favourite colour: "Blue"
+//10.Current year: 2023
+//11.Number of followers on a social media platform: 1,000,000
+//12.Rating of a movie: 7.5
+//13.Person's blood type: 'A'
+//14.Title of a book: "To Kill a Mockingbird"
+//15.Number of employees in a company: 500
+//16.Time of an event: 2:30 PM
+//17.Name of a country: "United States"
+//18.Person's eye color: "Brown"
+//19.Person's birthplace: "New York City"
+//20. Distance between two cities: 200.5
